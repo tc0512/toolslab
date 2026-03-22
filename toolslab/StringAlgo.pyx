c64ba46cfef5455e288dc5808e1a40e5a34da65e
tc0512 <tancheng_0812@qq.com>
2026-03-22 13:32:44 +0000

    StringAlgo添加修剪功能

--- toolslab/StringAlgo.pyx
+++ toolslab/StringAlgo.pyx
@@ -2,3 +2,11 @@ cpdef str to_upper(str string):
     return string.upper()
 cpdef str to_lower(str string):
     return string.lower()
+cpdef str trim(str string):
+    return string.strip()
+cpdef str trim_left(str string):
+    return string.lstrip()
+cpdef str trim_right(str string):
+    return string.rstrip()
+cpdef str trim_copy(str string):
+    return string.strip()
