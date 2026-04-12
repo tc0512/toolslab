e3d1ee292e44a037cb9897dcbbd8032773e7da0e
tc0512 <tancheng_0812@qq.com>
2026-04-12 09:19:31 +0000

    修复LexicalCast的bug

--- toolslab/LexicalCast.pyx
+++ toolslab/LexicalCast.pyx
@@ -5,5 +5,7 @@ cpdef int lexical_cast_int(str string): #字符串转整数
     return int(string)
 cpdef double lexical_cast_double(str string): #字符串转双精度浮点数
     return float(string)
-cpdef str lexical_cast(floating num): #数字转字符串
-    return str(num)
+def lexical_cast(num): #数字转字符串
+    if isinstance(num, (int, float)):
+        return str(num)
+    raise TypeError("Expected int or float")
