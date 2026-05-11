aeabe76fd5901dec4821c9f0f8869a062f46a436
tc0512 <tancheng_0812@qq.com>
2026-05-11 13:49:39 +0000

    Geometry子库添加多边形类

--- toolslab/Geometry.py
+++ toolslab/Geometry.py
@@ -21,3 +21,28 @@ class LineString:
         return total
     def size(self):
         return len(self.points)
+class Polygon:
+    def __init__(self, points):
+        self.points = points
+    def area(self):
+        n = len(self.points)
+        if n<3:
+            return 0
+        s = 0
+        for i in range(n):
+            x1, y1 = self.points[i].x, self.points[i].y
+            x2, y2 = self.points[(i+1)%n].x, self.points[(i+1)%n].y
+            s+=x1*y2-x2*y1
+        return abs(s)/2
+    def circumference(self):
+        n = len(self.points)
+        if n<2:
+            return 0
+        elif n==2:
+            return math.sqrt((self.points[1].x-self.points[0].x)**2+(self.points[1].y-self.points[0].y)**2)
+        C = 0
+        for i in range(n):
+            p1 = self.points[i]
+            p2 = self.points[(i+1)%n]
+            C+=math.sqrt((p2.x-p1.x)**2+(p2.y-p1.y)**2)
+        return C
